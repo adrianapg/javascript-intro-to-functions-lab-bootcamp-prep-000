9316a562b30cf85560f7c60a62a52fb3d4eda36d
Adriana Perez <adriana.perez.garcia@outlook.com>
2018-03-25 21:41:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,25 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHitoGran
+function sayHitoGrandma (string) {
+  if string === string.toLowerCase()
+  return "I can't hear you!"
+  else if string === string.toUpperCase()
+  return "YES INDEED!"
+  else 
+}
+
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
