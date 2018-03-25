c2f12347630360eec068672b98a2cc38cb08a369
Adriana Perez <adriana.perez.garcia@outlook.com>
2018-03-25 22:03:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHitoGrandma(string) {
   var uppercase = "HELLO!"
   var lowercase = "hello!"
   if (string === uppercase) {
-  return "I can't hear you!"}
-  else if (string === lowercase) {
-  return "YES INDEED!"}
-  else if (string === "I love you, Grandma.") { 
+  return "I can't hear you!"
+  } else if (string === lowercase){
+  return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") { 
   return "I love you, too." }
 }
 
